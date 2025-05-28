f9f958f0adb48c1fb6ccd169b6a2f6635afc109e
piotex <pkubon2@gmail.com>
2025-05-28 19:57:15 +0200

    front-end init

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { IdeaList } from "./components/IdeaList";
-import { Idea } from "./components/Idea";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [ideaList, setIdeaList] = useState([
-    { ideaName: "pomysł za 1.000.000", ideaCount: 13 },
-    { ideaName: "pomysł za 1.000", ideaCount: 7 },
-    { ideaName: "pomysł za 5.000", ideaCount: 23 },
-  ]);
-
   return (
-    <>
-      <IdeaList ideaList={ideaList}></IdeaList>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
